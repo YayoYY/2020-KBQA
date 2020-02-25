5af7460803b73bfe3ed401b77c97ac230132efba
YayoYY <346756297@qq.com>
2020-02-25 22:55:25 +0800

    v1.1

--- kbqa.py
+++ kbqa.py
@@ -1,5 +1,6 @@
 import sys
 from ner.ner_model import NERModel
+from sim.sim_model import SIMModel
 
 def ner():
     pass
@@ -11,16 +12,23 @@ def attribute_match():
     pass
 
 def online_kbqa():
-    print('offline kbqa start...')
+    print('online kbqa start...')
+    print('ner test...')
     model_ner = NERModel()
     ans = model_ner.online_predict("我是邺有！你好啊你好啊！")
+    print(ans)
+
+    print('sim test...')
+    model_sim = SIMModel()
+    ans = model_sim.predict("我是邺有！", "邺有")
     print(ans)
 
 def offline_kbqa():
     print('offline kbqa test')
 
 if __name__ == '__main__':
-    mode = sys.argv[1]
+    # mode = sys.argv[1]
+    mode = 'online'
 
     if mode == 'online':
         online_kbqa()
